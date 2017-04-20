564714079c0f62a6b664513733e758136726b6d6
Freezedr <freezedr@yandex.ru>
2017-04-20 16:56:03 +0300

    init commit

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,17 @@
-
+from collections import Counter
+import re
+import sys
 
 def load_data(filepath):
-    pass
-
+    with open(filepath) as f:
+        return f.read()
 
 def get_most_frequent_words(text):
-    pass
+    c = Counter(re.split("\W+", text))
+    return c.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = sys.argv[1]
+    text = load_data(filepath)
+    print(get_most_frequent_words(text))
