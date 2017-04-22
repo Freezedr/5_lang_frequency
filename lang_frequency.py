1c9bd83df8d3f7a9967ee4cadf680cb3df19049f
Freezedr <freezedr@yandex.ru>
2017-04-22 11:22:30 +0300

    Исправляю замечания
    Это были не переменные, а константы, переименовал согласно PEP-8
    https://www.python.org/dev/peps/pep-0008/#constants
    Добавил обработку несуществующего файла

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,21 +2,29 @@ from collections import Counter
 import re
 import sys
 
-word_amount = 10
-filepath_arg_position = 1
+WORD_AMOUNT = 10
+FILEPATH_ARG_POSITION = 1
+
 
 def load_data(filepath):
-    with open(filepath) as f:
-        return f.read()
+    try:
+        with open(filepath) as f:
+            return f.read()
+    except FileNotFoundError:
+        print("File not exist, try again")
+        return None
+
 
 def get_most_frequent_words(text):
     c = Counter(re.split("\W+", text))
-    return c.most_common(word_amount)
+    return c.most_common(WORD_AMOUNT)
+
 
 if __name__ == '__main__':
-    filepath = sys.argv[filepath_arg_position]
+    filepath = sys.argv[FILEPATH_ARG_POSITION]
     if filepath:
         text = load_data(filepath)
-        print(get_most_frequent_words(text))
+        if text:
+            print(get_most_frequent_words(text))
     else:
         print("Launch again with file path")
