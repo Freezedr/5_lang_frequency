18c28fb1fd44792d20f404faf9f9c7d0bfda71ea
Freezedr <freezedr@yandex.ru>
2017-10-05 22:02:46 +0300

    Update lang_frequency.py
    Ставлю переменные на место

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,10 +2,6 @@ from collections import Counter
 import re
 import sys
 
-WORD_AMOUNT = 10
-FILEPATH_ARG_POSITION = 1
-
-
 def load_data(filepath):
     try:
         with open(filepath) as f:
@@ -17,10 +13,12 @@ def load_data(filepath):
 
 def get_most_frequent_words(text):
     c = Counter(re.split("\W+", text))
+    WORD_AMOUNT = 10
     return c.most_common(WORD_AMOUNT)
 
 
 if __name__ == '__main__':
+    FILEPATH_ARG_POSITION = 1
     filepath = sys.argv[FILEPATH_ARG_POSITION]
     if filepath:
         text = load_data(filepath)
